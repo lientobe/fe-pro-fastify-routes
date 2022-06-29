4093881181a8f97f04612a085455ae6e02a0ddb3
Anzhelika <lientobe@gmail.com>
2022-06-29 18:49:48 +0300

    1st try

--- src/index.js
+++ src/index.js
@@ -11,4 +11,45 @@ fastify.register(import('@fastify/multipart'), {
 });
 fastify.register(import('@fastify/cookie'));
 
+fastify.post('/uppercase', (request, reply) => {
+  const body = request.body.toUpperCase();
+  if (body.includes('FUCK')) {
+    return reply.status(403).send('unresolved');
+  } else {
+    return reply.send(body);
+  }
+});
+
+fastify.post('/lowercase', (request, reply) => {
+  const body = request.body.toLowerCase();
+  if (body.includes('fuck')) {
+    return reply.status(403).send('unresolved');
+  } else {
+    return reply.send(body);
+  }
+});
+
+fastify.get('/user/:id', (request, reply) => {
+  const id = request.params.id;
+  if (users[id]) {
+    return reply.send(users[id]);
+  } else {
+    reply.status(400).send('User not exist');
+  }
+});
+
+fastify.get('/users', (request, reply) => {
+  const usersArray = Object.values(users);
+  let { filter, value } = request.query;
+  if(!filter || !value) {
+    return reply.send(usersArray);
+  }
+  else {
+    const filteredUsers = usersArray.filter((user) => {
+      return user[filter].toString() === value;
+    });
+    return reply.send(filteredUsers);
+  }
+});
+
 export default fastify;
